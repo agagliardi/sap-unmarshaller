ccc24f4a99be29140de9a8e14d19aa5594da936e
Antonio Gagliardi <antonio.gagliardi@gmail.com>
2020-08-05 15:59:03 +0200

    initial

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Run with:
+mvn clean install exec:java -Dsap-binary-folder=__SAP folder with libsapjco3.so sapjco3.jar__
+
+For Java 11 uncomment xml deps in pom.xml
+
---
+++ src/main/java/com/redhat/gss/fuse7/sap/Test.java
@@ -0,0 +1,7 @@
+import org.fusesource.camel.component.sap.converter.StructureConverter;
+
+public class Test{
+
+    public static void main(String[] args) {
+    }
+}
